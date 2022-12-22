4cbfe260225a32d99cc4620fb6f69eda4be09f97
Zinaida Budilova <budilowa.zinaida@gmail.com>
2022-12-22 22:57:19 +0300

    task 8 done

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -1,12 +1,11 @@
 package tasks;
 
 import common.Person;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,65 +22,58 @@ public class Task8 {
 
   private long count;
 
-  //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
-  public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
+  // Изменила проверку и добавила фильтрацию на null, перенесла пропуск элемента в stream, изменила название
+  public List<String> getPersonsFirstNames(List<Person> persons) {
+    if (persons == null) {
+      return List.of();
     }
-    persons.remove(0);
-    return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
+    return persons.stream()
+            .skip(1)
+            .filter(Objects::nonNull)
+            .map(Person::getFirstName)
+            .filter(Objects::nonNull)
+            .toList();
   }
 
-  //ну и различные имена тоже хочется
+  // Переписала с использованием предыдущей функции
   public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+    return new HashSet<>(getPersonsFirstNames(persons));
   }
 
-  //Для фронтов выдадим полное имя, а то сами не могут
-  public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.getSecondName() != null) {
-      result += person.getSecondName();
+  // Переписала как stream, добавила middle name, изменила название
+  public String getPersonFullName(Person person) {
+    if (person == null) {
+      return "";
     }
-
-    if (person.getFirstName() != null) {
-      result += " " + person.getFirstName();
-    }
-
-    if (person.getSecondName() != null) {
-      result += " " + person.getSecondName();
-    }
-    return result;
+    return Stream.of(person.getSecondName(), person.getFirstName(), person.getMiddleName())
+            .filter(Objects::nonNull)
+            .collect(Collectors.joining(" "));
   }
 
-  // словарь id персоны -> ее имя
-  public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.getId())) {
-        map.put(person.getId(), convertPersonToString(person));
-      }
+  // Переделала как stream, изменила название
+  public Map<Integer, String> getMapPersonIdToFullName(Collection<Person> persons) {
+    if (persons == null) {
+      return Map.of();
     }
-    return map;
+    return persons.stream()
+            .collect(Collectors.toMap(
+                    Person::getId,
+                    this::getPersonFullName,
+                    (existedName, newName) -> existedName)
+            );
   }
 
-  // есть ли совпадающие в двух коллекциях персоны?
-  public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
+  // Переделала как stream, подправила название
+  public boolean haveSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
+    if (persons1 == null || persons2 == null) {
+      return false;
     }
-    return has;
+    return persons1.stream()
+            .anyMatch(new HashSet<>(persons2)::contains);
   }
 
-  //...
-  public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+  // Убрала счётчик, дополнила название
+  public long countEvenNumbers(Stream<Integer> numbers) {
+    return numbers.filter(num -> num % 2 == 0).count();
   }
 }
